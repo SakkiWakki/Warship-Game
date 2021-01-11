f7599f4d8331aaa3e00079b41c86bcd6358bbc90
Jing <trashyiiiiiii@gmail.com>
2021-01-11 02:24:43 -0500

    no message

--- src/com/sakkiwakki/warshipgame/Main.java
+++ src/com/sakkiwakki/warshipgame/Main.java
@@ -1,13 +1,8 @@
 package com.sakkiwakki.warshipgame;
 	
 import com.sakkiwakki.warshipgame.view.ViewManager;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
